3f2849e104002e321421253aee2a1bdb995d5705
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-06 05:00:59 +0930

    Feat: Added second route for products page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
 function App () {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <HomePage />
+    },
+    {
+      path: '/products',
+      element: <ProductsPage />
     }
   ]);
   return <RouterProvider router={router} />;
