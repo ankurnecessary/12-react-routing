6311cc1616af75e7d3d9cd48fbe959e9bc67d9e9
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-07 04:07:23 +0930

    Added a root component so that we need not to repeat the same header again and again with links on all the pages.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import Root from './pages/Root';
 
 function App () {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <HomePage />
-    },
-    {
-      path: '/products',
-      element: <ProductsPage />
+      element: <Root />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />
+        },
+        {
+          path: '/products',
+          element: <ProductsPage />
+        }
+      ]
     }
   ]);
   return <RouterProvider router={router} />;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainNavigation from '../components/MainNavigation';
+import { Outlet } from 'react-router-dom';
+import classes from './Root.module.css';
+
+function Root () {
+  return (
+    <>
+      <MainNavigation />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+export default Root;
