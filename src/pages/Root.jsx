9b2bc9b1f3affeb2e32babde4cbb000467f6e7e9
Ankur Gupta <ankurnecessary@gmail.com>
2024-05-07 04:25:44 +0930

    Some CSS related modifications.

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import MainNavigation from '../components/MainNavigation';
 import { Outlet } from 'react-router-dom';
-import classes from './Root.module.css';
 
 function Root () {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
